e1859bafe219e66db894ac24dde78559384204cc
Ukropchik <vova.chakalov.2003@gmail.com>
2023-03-25 17:25:01 +0300

    Добавил героя и злодея

---
+++ git/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git
+{
+    internal class Enemy
+    {
+        //супер пупер злой чел
+    }
+}
